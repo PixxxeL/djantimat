c6aa6540ff01bf643ec39cad7fdeec3c2cd560fc
Ivan Sergeev <ivan.n.sergeev@gmail.com>
2024-01-08 09:31:42 +0500

    Bump version

--- djantimat/__init__.py
+++ djantimat/__init__.py
@@ -5,7 +5,7 @@ Detect dirty slang in russian texts and process it.
 Develope for Django 1.7 but may use it without this framework partly.
 """
 
-VERSION = (0, 1, 3,)
+VERSION = (0, 1, 4,)
 __version__ = '.'.join(map(str, VERSION))
 
 try:
